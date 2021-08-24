41dbe4563f1c065fecbb5f33d8e317696553bda1
ShadiFgh <shadifarokhghate@gmail.com>
2021-08-24 12:49:21 +0430

    update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('breast__cancer.csv')
 
@@ -37,3 +38,52 @@ def theta(coefs):
     coefs = coefs - (learning_rate * total)
 
     return coefs
+
+
+c = []
+lr = []
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+for i in range(1000):
+    if i % 10 == 0:
+        learning_rate *= 0.995
+        lr.append(learning_rate)
+    coefs = theta(coefs)
+    co = cost(coefs, X_train, y_train)[0]
+    print(f"LR: {learning_rate} Lv: {i + 1} ==> Cost: {co}")
+    c.append(co)
+
+print("============================================================================")
+
+print("b:", coefs[0][0])
+print("W:", coefs[0][1:])
+
+predict = []
+
+for x in X_test:
+    y_hat = h(coefs, x)
+    if y_hat >= 0.5:
+        predict.append(1)
+    else:
+        predict.append(0)
+
+s = 0
+for idx, i in enumerate(predict):
+    if i == y_test[idx][0]:
+        s += 1
+
+print(f"Correct predictions {s} out of {len(predict)}.")
+
+print("My Score:", s / len(predict))
+
+
+plt.xlabel("Iteration")
+plt.ylabel("Cost")
+plt.plot(c)
+plt.show()
+
+plt.xlabel("Iteration")
+plt.ylabel("LR")
+plt.plot(lr)
+plt.show()
